25c69c82003a3d38acf2de2d6ae8a90350b88127
hoang.le <leehoang@gmail.com>
2023-05-09 00:04:16 +0700

    sua file

--- home/home.js
+++ home/home.js
@@ -1,19 +1,59 @@
-const settingsmenu = document.querySelector(".settings-menu");
-const darkBtn = document.getElementById("dark-btn");
+function uploadImage(e) {
+  let fbBucketName = "images";
+  let uploader = document.getElementById("uploader");
+  let file = e.target.files[0];
+  let storageRef = firebase.storage().ref(`${fbBucketName}/${file.name}`);
+  let uploadTask = storageRef.put(file);
+  uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      function (snapshot) {
+        uploader.value =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED:
+            break;
+          case firebase.storage.TaskState.RUNNING:
+            break;
+        }
+      },
+      function (error) {
+        switch (error.code) {
+          case "storage/unauthorized":
+            break;
+          case "storage/canceled":
+            break;
+          case "storage/unknown":
+            break;
+        }
+      },
+      function () {
+        let downloadURL = uploadTask.snapshot.downloadURL;
+        console.log(downloadURL);
+        document.getElementById(
+            "imgDiv"
+        ).innerHTML = `<img src="${downloadURL}" alt="" style="width: 450px; height: 180px" >`;
+        document.getElementById("image").value = downloadURL;
+      }
+  );
+}
+
 
 function settingsMenuToggle() {
+  const settingsmenu = document.querySelector(".settings-menu");
   settingsmenu.classList.toggle("settings-menu-height");
 }
-darkBtn.onclick = function () {
+
+function changeBackground(){
+  const darkBtn = document.getElementById("dark-btn");
   darkBtn.classList.toggle("dark-btn-on");
   document.body.classList.toggle("dark-theme");
-
   if (localStorage.getItem("theme") == "light") {
     localStorage.setItem("theme", "dark");
   } else {
     localStorage.setItem("theme", "light");
   }
-};
+}
+
 
 if (localStorage.getItem("theme") == "light") {
   darkBtn.classList.remove("dark-btn-on");
@@ -26,14 +66,13 @@ if (localStorage.getItem("theme") == "light") {
 }
 
 
-const input = document.getElementById("input-box");
-const box = document.querySelector(".post-box");
-const closeButton = document.getElementById('closeButton');
+function showModal(){
+  const box = document.querySelector(".post-box");
+  box.style.display = "block";
+}
 
-input.addEventListener("click",() => {
- box.style.display = "block";
-});
+function hideModal(){
+  const box = document.querySelector(".post-box");
+  box.style.display = "none";
+}
 
-closeButton.addEventListener('click', () => {
- box.style.display = "none";
-});
--- home/index.js
+++ home/index.js
@@ -199,7 +199,7 @@ function showHomePage(user, friendList) {
          <span>Thêm vào bài đăng</span>
           <input type="hidden" class="form-control"  name="image"placeholder="image" id="image" />
             <input
-            
+                style="display: none;"
                 type="file"
                 id="fileButton"
                 onchange="uploadImage(event)"
@@ -484,83 +484,5 @@ function showHomePage(user, friendList) {
 <script src="./home/home.js" ></script>
  `;
   $("#body").html(html);
-}
-
-function uploadImage(e) {
-  let fbBucketName = "images";
-  let uploader = document.getElementById("uploader");
-  let file = e.target.files[0];
-  let storageRef = firebase.storage().ref(`${fbBucketName}/${file.name}`);
-  let uploadTask = storageRef.put(file);
-  uploadTask.on(
-      firebase.storage.TaskEvent.STATE_CHANGED,
-      function (snapshot) {
-        uploader.value =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        switch (snapshot.state) {
-          case firebase.storage.TaskState.PAUSED:
-            break;
-          case firebase.storage.TaskState.RUNNING:
-            break;
-        }
-      },
-      function (error) {
-        switch (error.code) {
-          case "storage/unauthorized":
-            break;
-          case "storage/canceled":
-            break;
-          case "storage/unknown":
-            break;
-        }
-      },
-      function () {
-        let downloadURL = uploadTask.snapshot.downloadURL;
-        console.log(downloadURL);
-        document.getElementById(
-            "imgDiv"
-        ).innerHTML = `<img src="${downloadURL}" alt="" style="width: 450px; height: 180px" >`;
-        document.getElementById("image").value = downloadURL;
-      }
-  );
-}
-
-
-function settingsMenuToggle() {
-  const settingsmenu = document.querySelector(".settings-menu");
-  settingsmenu.classList.toggle("settings-menu-height");
-}
-
-function changeBackground(){
-  const darkBtn = document.getElementById("dark-btn");
-  darkBtn.classList.toggle("dark-btn-on");
-  document.body.classList.toggle("dark-theme");
-  if (localStorage.getItem("theme") == "light") {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
-}
-
-
-if (localStorage.getItem("theme") == "light") {
-  darkBtn.classList.remove("dark-btn-on");
-  document.body.classList.remove("dark-theme");
-} else if (localStorage.getItem("theme") == "dark") {
-  darkBtn.classList.add("dark-btn-on");
-  document.body.classList.add("dark-theme");
-} else {
-  localStorage.setItem("theme", "light");
-}
-
-
-function showModal(){
-  const box = document.querySelector(".post-box");
-  box.style.display = "block";
-}
-
-function hideModal(){
-  const box = document.querySelector(".post-box");
-   box.style.display = "none";
 }
 
