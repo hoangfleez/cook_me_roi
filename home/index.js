4977eb2f60b2edf0ecda9bfe7c29b8ece7ca1b5c
DangTrieu02 <dangtrieu.cdht@gmail.com>
2023-05-09 09:45:52 +0700

    hehe

--- home/index.js
+++ home/index.js
@@ -20,20 +20,79 @@ function getUser(){
                       'Authorization':  'Bearer ' + localStorage.getItem('authorization')
                   }, 
                   success(friendList){
-
+                    $.ajax({
+                      type: "GET",
+                      url:`http://localhost:3000/post`,
+                        headers:{
+                            "Content-Type": "application/json",
+                            'Authorization':  'Bearer ' + localStorage.getItem('authorization')
+                        },
+                        success(postList){
+                          let posts=``
+                          postList.map(item => {
+                            posts+=`<div class="posts">
+                            <div class="posts-top">
+                              <div class="posts-top-user">
+                                <img
+                                  src="${item.avatar}"
+                                  alt=""
+                                />
+                                <div class="posts-top-user-name">
+                                  <a><span>${item.name}</span></a>
+                                  <a 
+                                    ><small
+                                      >Public <i class="fa-solid fa-earth-americas"></i></small
+                                  ></a>
+                                </div>
+                              </div>
+                              <div class="posts-top-ntm">
+                                <i class="fa-solid fa-ellipsis"></i>
+                              </div>
+                            </div>
+                            <div class="posts-main">
+                              <div class="posts-main-text">
+                                <span>
+                                ${item.postContent}
+                                </span>
+                              </div>
+                              <div class="posts-main-img">
+                                <img
+                                  src="${item.postImage}"
+                                  alt=""
+                                />
+                              </div>
+                            </div>
+                            <div class="posts-bottom" >
+                              <div class="posts-bottom-like" onclick="like(${item.id})">
+                                <i class="fa-light fa-thumbs-up" id="like"></i>
+                                <span>100</span>
+                              </div>
+                              <div class="posts-bottom-cmt" onclick="showCommentBox(${item.id}.)">
+                                <i class="fa-regular fa-message"></i>
+                                <span> 0 </span>
+                              </div>
+                              <div class="posts-bottom-share">
+                                <i class="fa-light fa-share"></i>
+                                <span></span>
+                              </div>
+                            </div>
+                          </div>`
+                          })
+                          showHomePage(data[0],friendList,posts);
+                        }
                     
 
 
 
-                      showHomePage(data[0],friendList);
-                  }
-              })
+                      
+                  })
+              }})
       }
   })
 }
 
 
-function showHomePage(user, friendList) {
+function showHomePage(user, friendList,posts) {
   console.log(user);
   console.log(friendList);
   let friendHtml=``
@@ -414,58 +473,10 @@ function showHomePage(user, friendList) {
    </div>
 
    <div class="post-container">
-    <div class="posts">
-            <div class="posts-top">
-              <div class="posts-top-user">
-                <img
-                  src="https://haycafe.vn/wp-content/uploads/2021/11/Anh-avatar-dep-chat-lam-hinh-dai-dien.jpg"
-                  alt=""
-                />
-                <div class="posts-top-user-name">
-                  <a href=""><span>bawp dun</span></a>
-                  <a href=""
-                    ><small
-                      >Public <i class="fa-solid fa-earth-americas"></i></small
-                  ></a>
-                </div>
-              </div>
-              <div class="posts-top-ntm">
-                <i class="fa-solid fa-ellipsis"></i>
-              </div>
-            </div>
-            <div class="posts-main">
-              <div class="posts-main-text">
-                <span
-                  >Hôm nay trời đẹp quá
-                  <h1>
-                    asdasadasdasdljsalkjdlajsdlkjasljdlasjdlkjasljdlasjdljasldjasjdljasldjasljdljsaldjasljd
-                  </h1>
-                </span>
-              </div>
-              <div class="posts-main-img">
-                <img
-                  src="https://haycafe.vn/wp-content/uploads/2021/11/Anh-avatar-dep-chat-lam-hinh-dai-dien.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div class="posts-bottom" >
-              <div class="posts-bottom-like" onclick="like()">
-                <i class="fa-light fa-thumbs-up" id="like"></i>
-                <span>100</span>
-              </div>
-              <div class="posts-bottom-cmt" onclick="showCommentBox()">
-                <i class="fa-regular fa-message"></i>
-                <span>2</span>
-              </div>
-              <div class="posts-bottom-share">
-                <i class="fa-light fa-share"></i>
-                <span>10</span>
-              </div>
-            </div>
-          </div>
+   ${posts}
+    
    </div>
- </div>
+ </div>'
 
  <!-- -----------------right-sidebar------------------- -->
  <div class="right-sidebar">
@@ -529,3 +540,89 @@ function showHomePage(user, friendList) {
   $("#body").html(html);
 }
 
+function uploadImage(e) {
+  let fbBucketName = "images";
+  let uploader = document.getElementById("uploader");
+  let file = e.target.files[0];
+  let storageRef = firebase.storage().ref(`${fbBucketName}/${file.name}`);
+  let uploadTask = storageRef.put(file);
+  uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      function (snapshot) {
+        uploader.value =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED:
+            break;
+          case firebase.storage.TaskState.RUNNING:
+            break;
+        }
+      },
+      function (error) {
+        switch (error.code) {
+          case "storage/unauthorized":
+            break;
+          case "storage/canceled":
+            break;
+          case "storage/unknown":
+            break;
+        }
+      },
+      function () {
+        let downloadURL = uploadTask.snapshot.downloadURL;
+        console.log(downloadURL);
+        document.getElementById(
+            "imgDiv"
+        ).innerHTML = `<img src="${downloadURL}" alt="" style="width: 450px; height: 180px" >`;
+        document.getElementById("image").value = downloadURL;
+      }
+  );
+}
+
+
+function settingsMenuToggle() {
+  const settingsmenu = document.querySelector(".settings-menu");
+  settingsmenu.classList.toggle("settings-menu-height");
+}
+
+function changeBackground(){
+  const darkBtn = document.getElementById("dark-btn");
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
+
+
+
+
+
+// like comment
+function like(){
+
+  let icon = document.getElementById("like");
+  let likes = icon.getAttribute("class");
+  let newClassLikes = (likes === 'fa-light fa-thumbs-up') ? 'fa-solid fa-thumbs-up' : 'fa-light fa-thumbs-up'
+  icon.setAttribute("class", newClassLikes)
+}
+function likeInBox(){
+
+  let icon = document.getElementById("likeInBox");
+  let likes = icon.getAttribute("class");
+  let newClassLikes = (likes === 'fa-light fa-thumbs-up') ? 'fa-solid fa-thumbs-up' : 'fa-light fa-thumbs-up'
+  icon.setAttribute("class", newClassLikes)
+}
